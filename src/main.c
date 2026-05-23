79a08d4f9c1d4f73bdc2c9d757c88104ac13956f
Stockmite <rojogreenazul2@gmail.com>
2026-05-23 08:18:25 -0300

    I reckon the "PlaySound" function shall suffice for the time being. It appears to accept primarily .wav files, but I'd rather get a working prototype as soon as possible and this is the path of least resistance

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+
+#include "morsetranslation.h"
+
+int main() {
+
+    PlaySound("audiofiles/A.wav", NULL, SND_FILENAME);
+
+}
