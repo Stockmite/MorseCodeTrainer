a7892f213b397c65dee77ca7b1977bcbd24bd70d
Stockmite <rojogreenazul2@gmail.com>
2026-05-24 16:43:22 -0300

    Well, this is the program. Not very glamorous, but a starting point for the application. I'll come back to this in a future date

--- src/main.c
+++ src/main.c
@@ -5,8 +5,31 @@
 
 #include "morsetranslation.h"
 
+#define alphabetlen 36
+
 int main() {
 
-    PlaySound("audiofiles/A.wav", NULL, SND_FILENAME);
+    char response = 'a';
+    
+    while (true) {
+
+        srand(time(NULL));
+        int ind = rand() % alphabetlen;
+        char letter = toupper(alphabet[ind]);
+
+        char filename[17];
+        sprintf(filename, "audiofiles/%c.wav", letter);
+        PlaySound(filename, NULL, SND_FILENAME);
+
+        scanf("%c", &response);
+
+        if (toupper(response) == letter) {
+            printf("CORRECT! :D \n");
+        } else {
+            printf("Incorrect D:  the answer is %c \n", letter);
+        }
+
+        fflush(stdin);
+    }
 
 }
